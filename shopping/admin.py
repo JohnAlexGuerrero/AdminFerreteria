515247329c9ab102bce878231aba491034f4a796
jalexguerrero <johnalexguerrero@gmail.com>
2023-07-24 17:18:49 -0500

    admininvoices: add days column of expiration

--- shopping/admin.py
+++ shopping/admin.py
@@ -1,12 +1,23 @@
 from django.contrib import admin
+from datetime import datetime
 
 from shopping.models import Providers, Invoice, ProductInvoice
 
+# admin invoices
 class InvoicesAdmin(admin.ModelAdmin):
-    list_display = ('number','provider','created_at','expiration_at','total')
+    list_display = ('number','provider','created_at','expiration_at','total','days')
+    
+    ordering = ('created_at',)
+    
+    def days(self, obj):
+        now = datetime.now()
+        days = (datetime.strptime(str(obj.expiration_at), "%Y-%m-%d") - now).days
+        return days
 
 class InventoryAdmin(admin.ModelAdmin):
     list_display = ('invoice','product','quantity','price')
+    
+
 
 admin.site.register(Providers) 
 admin.site.register(Invoice, InvoicesAdmin) 
